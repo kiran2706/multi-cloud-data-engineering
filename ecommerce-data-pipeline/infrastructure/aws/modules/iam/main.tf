919436f5af7dfc2e0d6b3730fad5c09c47c91347
kiran2706 <kiran27rsecure@gmail.com>
2025-05-29 19:40:11 +0530

    Created a policy to write logs and apply to lambda

--- ecommerce-data-pipeline/infrastructure/aws/modules/iam/main.tf
+++ ecommerce-data-pipeline/infrastructure/aws/modules/iam/main.tf
@@ -39,8 +39,36 @@ resource "aws_iam_policy" "put_to_kinesis" {
   })
 }
 
+# IAM Policy: Allows Lambda to write logs to CloudWatch Logs
+resource "aws_iam_policy" "lambda_cloudwatch_logs" {
+  name        = "LambdaCloudWatchLogsPolicy"  # Name of the IAM policy
+  description = "Allows Lambda functions to write logs to CloudWatch"
+
+  # Define the permissions this policy grants
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",  # This is an allow policy
+        Action = [         # List of allowed CloudWatch Logs actions
+          "logs:CreateLogGroup",    # Allows creation of a log group
+          "logs:CreateLogStream",   # Allows creation of a log stream in the group
+          "logs:PutLogEvents"       # Allows pushing actual logs to the log stream
+        ],
+        Resource = "arn:aws:logs:*:*:*"  # Wildcard to allow writing logs in any region and account
+      }
+    ]
+  })
+}
+
 # Attach policy to role
 resource "aws_iam_role_policy_attachment" "attach_kinesis_policy" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.put_to_kinesis.arn
+}
+
+# Attach CloudWatch Logs policy
+resource "aws_iam_role_policy_attachment" "attach_lambda_cloudwatch_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_cloudwatch_logs.arn
 }
